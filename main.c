860e4cc2e3bad86c86cd994e42c9aeb26556f363
Ryo Kinjo <ryokinjo@cmail.carleton.ca>
2019-12-26 15:00:40 -0500

    Removed argument requirements

--- main.c
+++ main.c
@@ -3,12 +3,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-
-    if(argc != 3){
-        printf("Usage: fipe <bin1> <bin2>");
-        return 1;
-    }
+int main(){
 
     return 0;
 }
