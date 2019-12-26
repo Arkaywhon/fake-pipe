23b693f85222b5c2c7b1d0e05ed2443d9ba4f4ba
Ryo Kinjo <ryokinjo@cmail.carleton.ca>
2019-12-26 15:15:35 -0500

    Implemented forking and execve functionality

--- main.c
+++ main.c
@@ -12,5 +12,21 @@ int main(int argc, char *argv[], char *envp[]){
 
 int binRunner(char *name, char *path, int in_file, int out_file, char *envp[]){
 
-    
+    int pid;
+    pid = fork();
+    if(pid == 0){
+
+        char *args[2];
+        args[0] = name;
+        args[1] = NULL;
+
+        execve(path, args, envp);
+        printf("Error with execve\n");
+        return 1;
+    }else if(pid > 0){
+        wait(NULL);
+    }else{
+        printf("Error forking\n");
+        return 1;
+    }
 }
