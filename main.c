509e11f31e2aa71d73a6de491f89c1511cdd4f7a
Ryo Kinjo <ryokinjo@cmail.carleton.ca>
2019-12-27 11:50:28 -0500

    Implemented standard in/out file desc. redirection

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int binRunner(char *, char *, int, int, char **);
 
@@ -13,8 +15,22 @@ int main(int argc, char *argv[], char *envp[]){
 int binRunner(char *name, char *path, int in_file, int out_file, char *envp[]){
 
     int pid;
+    int in_fd, out_fd;
+
     pid = fork();
     if(pid == 0){
+
+        if(in_file != NULL){
+            in_fd = open(in_file, O_RDWR);
+            dup2(in_fd, 0);
+            close(in_fd);
+        }
+
+        if(out_file != NULL){
+            out_fd = open(out_file, O_CREAT|O_RDWR);
+            dup2(out_fd, 1);
+            close(out_fd);
+        }
 
         char *args[2];
         args[0] = name;
